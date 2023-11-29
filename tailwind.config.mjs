8e14fc7b27c5edc969312b404cf0839d1f72f41a
Edoardo Baldi <edoardob90@gmail.com>
2023-11-29 21:22:54 +0100

    Change fonts of bold text and notes
    Valkyrie Caps for bold text in article
    Heliotrope + Caps for margin and foot notes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,9 +7,12 @@ export default {
 		extend: {
 			fontFamily: {
 				serif: ['valkyrie', ...defaultTheme.fontFamily.serif],
+				caps: ['valkyrie_caps'], // small caps for body text
 				sans: ['concourse', ...defaultTheme.fontFamily.sans],
 				mono: ['iosevka_fixed', ...defaultTheme.fontFamily.mono],
-				headline: ['concourse_caps'],
+				headline: ['concourse_caps'], // font for headlines
+				outer: ['heliotrope'], // font for notes, margin notes, etc.
+				outerCaps: ['heliotrope_caps'], // small caps for notes, margin notes, etc.
 			},
 			listStyleType: {
 				circle: 'circle',
